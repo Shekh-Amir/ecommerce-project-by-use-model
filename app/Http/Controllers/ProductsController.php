22079223dfca831a6a537e005c5901ae61765c2c
Shekh-Amir <amirulapee.ru@gmail.com>
2018-11-15 00:42:56 +0600

    Add existing file

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -691,6 +691,28 @@ class ProductsController extends Controller
           $order->payment_method=$data['payment_method'];
           $order->grand_total=$data['grand_total'];
           $order->save();
+
+
+          $order_id=DB::getPdo()->lastInsertId();
+
+          $cartProducts=DB::table('cart')->where(['user_email'=>$user_email])->get();
+
+          foreach ($cartProducts as $pro) {
+
+            $cartPro=new OrdersProduct;
+            $cartPro->order_id=$order_id;
+            $cartPro->user_id=$user_id;
+            $cartPro->product_id=$pro->product_id;
+            $cartPro->product_code=$pro->product_code;
+            $cartPro->product_name=$pro->product_name;
+            $cartPro->product_color=$pro->product_color;
+            $cartPro->product_size=$pro->size;
+            $cartPro->product_price=$pro->price;
+            $cartPro->product_qty=$pro->quantity;
+       
+            $cartPro->save();
+           
+          }
          
 
         }
